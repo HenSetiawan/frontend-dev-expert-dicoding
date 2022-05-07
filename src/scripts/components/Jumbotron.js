1694ac1cffa844f67f24eeac6e39dabb555ac853
hensetiawan <hendystyawan74@gmail.com>
2022-05-07 18:54:24 +0700

    fix responsive hero image

--- src/scripts/components/Jumbotron.js
+++ src/scripts/components/Jumbotron.js
@@ -6,17 +6,20 @@ class Jumbotron extends HTMLElement {
   render() {
     this.innerHTML = `
       <section class="hero mt-2">
-      <img 
-        src='./images/hero-image-large.jpg' 
-        alt="cafe menu"></img>
-      <div class="hero-description">
+        <picture>
+          <source media="(max-width: 600px)" srcset="./images/hero-image-small.jpg">
+         <img 
+            src='./images/hero-image-large.jpg' 
+            alt="dirt rally poster"></img>
+        </picture>
+       <div class="hero-description">
         <h1>Good Food is A Good Mood.</h1>
         <p>
           Food is the ingredient that bind us together! And nothing brings
           people together like a Good Food!
         </p>
         <a href="#main-menu" class="btn">Let's Eat it!</a>
-      </div>
+        </div>
     </section>
       `;
   }
